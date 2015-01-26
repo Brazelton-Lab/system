c36a15df3a47b8d1f72e9c06d7b0ab646923e3eb
Christopher Thornton <christopher.thornton@utah.edu>
2015-01-26 13:49:46 -0700

    Fix to comply with standard conventions and add support for multiple program input

--- utils.py
+++ utils.py
@@ -19,21 +19,22 @@ import argparse
 
 def argument_parser():
     parser = argparse.ArgumentParser(description="Display information about "
-                                    "the available bioinformatics programs on "
-                                    "the server. If no arguments given, will "
-                                    "list all of the programs (with a short "
-                                    "description)")
+                                     "the available bioinformatics programs "
+                                     "on the server. If no arguments given, "
+                                     "will list all of the programs (with a "
+                                     "short description)")
     parser.add_argument('-n', '--name',
-                       type=str,
-                       metavar='PROGRAM',
-                       help="for obtaining detailed information about a "
-                       "program. The name of the program is case-sensitive.")
+                        type=str,
+                        metavar='PROGRAM',
+                        nargs='+',
+                        help="for obtaining detailed information about a "
+                        "program. The name of the program is case-sensitive.")
     parser.add_argument('-p', '--prev_vers',
-                       action='store_const',
-                       const='previous versions',
-                       help="list all of versions of the specified program "
-                       "that were used on the server in the past (must be used "
-                       "with the --name option)")
+                        action='store_const',
+                        const='previous versions',
+                        help="list all of versions of the specified program "
+                        "that were used on the server in the past (must be "
+                        "used with the --name option)")
     parser.add_argument('-f', '--full_desc',
                         action='store_const',
                         const='description',
@@ -73,6 +74,34 @@ def display_progs(data):
         else:
             print_out("{:<30}{}".format(col_one, col_two), subsequent=indent)
 
+def prog_detail(prog, data, mods):
+    if prog in data:
+        version = data[prog]["version"]
+        if version:
+            print_out("{}(v.{}):".format(prog, version))
+        else:
+            print_out(prog)
+        for modifier in sorted(mods):
+            category = mods[modifier]
+            if category:
+                cat_indent = ' ' * 4
+                print_out(category + ':', initial=cat_indent,
+                          subsequent=cat_indent)
+                detail_indent = ' ' * 8
+                if type(data[prog][category]) == type(list()):
+                    line = ', '.join(sorted(data[prog][category]))
+                else:
+                    line = data[prog][category]
+                print_out(line, initial=detail_indent,
+                          subsequent=detail_indent)
+    else:
+        output = ("{} is not installed on the server. Please verify that the "
+                  "program you are searching for is installed on the server, "
+                  "and/or not misspelled, by usings \"utils\" without "
+                  "command-line arguments".format(prog))
+        print_out(output)
+        sys.exit(1)
+
 def print_out(line, width=79, initial='', subsequent=''):
     output = textwrap.fill(line, width, initial_indent=initial, 
                           subsequent_indent=subsequent)
@@ -80,34 +109,15 @@ def print_out(line, width=79, initial='', subsequent=''):
 
 def main():
     args = argument_parser().parse_args()
+    programs = args.name
     utils = "/usr/local/etc/utils.txt"
-    max_width = 79
     with open(utils, 'rU') as infile:
         json_data = json.load(infile)
-    if args.name:
-        prog = args.name
-        if prog in json_data:
-            version = json_data[prog]["version"]
-            if version:
-                print_out("{}(v.{}):".format(prog, version), max_width)
-            else:
-                print_out(prog, max_width)
-            arg_dict = vars(args)
-            del arg_dict['name']
-            for argument in sorted(arg_dict):
-                category = arg_dict[argument]
-                if category:
-                    cat_indent = ' ' * 4
-                    print_out(category + ':', max_width, cat_indent, cat_indent)
-                    detail_indent = ' ' * 8
-                    if type(json_data[prog][category]) == type(list()):
-                        line = ', '.join(sorted(json_data[prog][category]))
-                    else:
-                        line = json_data[prog][category]
-                    print_out(line, max_width, detail_indent, detail_indent)
-        else:
-            print("{} is not installed on the server".format(prog))
-            sys.exit(1)
+    if programs:
+        modifiers = vars(args)
+        del modifiers['name']
+        for prog in programs:
+            prog_detail(prog, json_data, modifiers)
     else: 
         display_progs(json_data)
 
