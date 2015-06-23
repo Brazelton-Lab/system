84989ac0aa5115b3d15de6f9f955a8569863a6dd
Christopher Thornton <christopher.thornton@utah.edu>
2015-06-23 10:57:39 -0600

    modify how utils accepts multiple arguments

--- utils.py
+++ utils.py
@@ -4,7 +4,8 @@ Parses a JSON-formatted file containing information on the available
 programs on the server and displays it as human-readable text. The user can 
 specify a program of interest as a command line argument and receive more 
 detail about the program in return, including previous version information, 
-dependencies, and a list of possible commands supplied by the program.
+dependencies, and a list of possible commands supplied by the program. Also
+includes functionality for editing the database.
 """
 
 from __future__ import print_function
@@ -20,91 +21,104 @@ import re
 
 def argument_parser():
     parser = argparse.ArgumentParser(description="Displays information about "
-                                     "the available bioinformatics programs "
-                                     "on the server. If no arguments given "
-                                     "with the list subcommand, will list all"
-                                     ". Various flags can be used to modify "
-                                     "the information output with the display "
-                                     "subcommand")
+        "the available bioinformatics programs on the server. If no arguments "
+        "given with the list subcommand, will list all. Various flags can be "
+        "used to modify the information output with the display subcommand")
     parent_parser = argparse.ArgumentParser(add_help=False)
-    parent_parser.add_argument('program', 
-                               metavar='PROGRAM', 
-                               help="program name")
+    parent_parser.add_argument('program',
+        metavar='PROGRAM', 
+        help="program name")
+    db_parser = argparse.ArgumentParser(add_help=False)
+    db_parser.add_argument('-b', '--database', metavar="DB",
+        default="/usr/local/etc/utils.txt",
+        type=io_check,
+        help="provide a custom JSON-formatted database file")
     subparsers = parser.add_subparsers(title="subcommands", 
-                                       help="exactly one of these commands is "
-                                       "required")
+        help="exactly one of these commands is required")
     # list-specific arguments
-    list_parser = subparsers.add_parser('list', 
-                                        help="list all available programs")
+    list_parser = subparsers.add_parser('list',
+        parents=[db_parser],
+        help="list all available programs")
     list_parser.set_defaults(func=prog_list)
     # edit-specific arguments
-    edit_parser = subparsers.add_parser('edit', parents=[parent_parser],
-                                        help="edit, append, or remove an entry "
-                                        "to the database of bioinformatics "
-                                        "programs")
-    edit_parser.add_argument('-v','--version', 
-                             metavar='VERSION',
-                             help="current version of the program (can be "
-                             "\"null\" if no version info available)")
+    edit_parser = subparsers.add_parser('edit',
+        parents=[parent_parser, db_parser],
+        help="edit, append, or remove an entry to the database")
+    edit_parser.add_argument('-v','--version',
+        metavar='VERSION',
+        help="current version of the program (can be \"null\" if no version "
+            "info available)")
     edit_parser.add_argument('-s', '--synopsis', 
-                             dest="description",
-                             metavar='DESCRITPTION', 
-                             help="program description")
+        dest="description",
+        metavar='DESCRITPTION', 
+        help="program description")
     edit_parser.add_argument('-p', '--prev',
-                        dest="previous versions",
-                        metavar='VERSION',
-                        nargs='+',
-                        help="list of former versions of the program used on the "
-                        "server, with last date used (ex: <version>(to <date>)")
+        metavar='VERSION [,VERSION,...]',
+        dest="previous versions",
+        type=parse_multiple_args,
+        help="comma-separated list of previous versions with last date used "
+            "(ex: <version>(to <date>)")
     edit_parser.add_argument('-c', '--commands',
-                        metavar='COMMAND',
-                        nargs='+',
-                        help="list of available commands provided by program")
-    edit_parser.add_argument('-i', '--installation',
-                        dest="installation method",
-                        metavar='METHOD',
-                        help="method used to install program")
+        metavar='COMMAND [,COMMAND,...]',
+        type=parse_multiple_args,
+        help="comma-separated list of commands provided by the program")
+    edit_parser.add_argument('-i', '--installation', metavar='METHOD',
+        dest="installation method",
+        help="method used to install the program")
     edit_parser.add_argument('-d', '--depends',
-                        dest="dependencies",
-                        metavar='DEPENDENCY',
-                        nargs='+',
-                        help="list of program dependencies")
+        metavar='DEP [,DEP,...]',
+        dest="dependencies",
+        type=parse_multiple_args,
+        help="comma-separated list of program dependencies")
     group_mode = edit_parser.add_argument_group('actions')
     exclusive_group = group_mode.add_mutually_exclusive_group(required=True)
     exclusive_group.add_argument('-a', '--append', 
-                       action='store_true', 
-                       help="add new entry to database")
+        action='store_true', 
+        help="add new entry to database")
     exclusive_group.add_argument('-e', '--edit', 
-                       action='store_true', 
-                       help="edit existing entry in database")
+        action='store_true', 
+        help="edit existing entry in database")
     exclusive_group.add_argument('-r', '--remove',
-                       action='store_true',
-                       help="remove existing entry from database")
+        action='store_true',
+        help="remove existing entry from database")
     edit_parser.set_defaults(func=prog_edit)
     # display-specific arguments
-    display_parser = subparsers.add_parser('display', parents=[parent_parser],
-                                          help="obtain detailed information "
-                                          "about a specific program")
+    display_parser = subparsers.add_parser('display',
+        parents=[parent_parser, db_parser],
+        help="obtain detailed information about a program")
     flag_group = display_parser.add_argument_group('flags')
     flag_group.add_argument('-p', '--prev',
-                        action='store_const',
-                        const='previous versions',
-                        help="list former versions of the program used on the "
-                        "server")
+        action='store_const',
+        const='previous versions',
+        help="list former versions of the program used on the server")
     flag_group.add_argument('-c', '--commands',
-                        action='store_const',
-                        const='commands',
-                        help="list available commands provided by program")
+        action='store_const',
+        const='commands',
+        help="list available commands provided by program")
     flag_group.add_argument('-i', '--installation',
-                        action='store_const',
-                        const='installation method',
-                        help="display method used to install program")
+        action='store_const',
+        const='installation method',
+        help="display method used to install program")
     flag_group.add_argument('-d', '--depends',
-                        action='store_const',
-                        const='dependencies',
-                        help="list program dependencies")
+        action='store_const',
+        const='dependencies',
+        help="list program dependencies")
     display_parser.set_defaults(func=prog_display)
     return parser
+
+def parse_multiple_args(arguments):
+    arguments = arguments.split(',')
+    return arguments
+
+def io_check(infile, mode='rU'):
+    try:
+        fh = open(infile, mode)
+    except IOError as e:
+        print(e)
+        sys.exit(1)
+    else:
+        fh.close()
+    return infile
 
 def test_matched(ci_prog, data):
     match = False
@@ -123,6 +137,13 @@ def display_info(first, second):
         else:
             print_out("{:<20}{}".format(first, second), subsequent=indent)
 
+def relevant_values(all_args, name, data):
+    given_args = []
+    for arg in all_args:
+        if arg in data[name] and all_args[arg]:
+            given_args.append(arg)
+    return given_args
+
 def prog_list(args, data):
     for program in sorted(data):
         version = data[program]["version"]
@@ -132,13 +153,6 @@ def prog_list(args, data):
             col_one = program
         col_two = data[program]["description"]
         display_info(col_one, col_two)
-
-def relevant_values(all_args, name, data):
-    given_args = []
-    for arg in all_args:
-        if arg in data[name] and all_args[arg]:
-            given_args.append(arg)
-    return given_args
 
 def prog_display(args, data):
     all_args = vars(args)
@@ -175,7 +189,6 @@ def prog_display(args, data):
         sys.exit(1)
 
 def prog_edit(args, data):
-    utils = "/home/cthornton/dev_projects/system/utils.txt"
     all_args = vars(args)
     match = test_matched(args.program, data)
     if args.remove:
@@ -224,28 +237,21 @@ def prog_edit(args, data):
             print_out("\"{}\" already exists in database. Use \"utils edit -e "
                       "<program>\" to modify an entry".format(args.program))
             sys.exit(1)
-    write_out(data, utils)
 
-def write_out(data, outfile):
-    try:
-        test_h = open(outfile, 'w')
-        test_h.close()
-    except IOError as e:
-        print(e)
-        sys.exit(1)
-    with open(outfile, 'w') as out:
-        out.write(json.dumps(data, sort_keys=True))
-    
+    utils = io_check(args.database, 'w')
+    with open(utils, 'w') as out_h:
+        out_h.write(json.dumps(data, sort_keys=True))
+
 def print_out(line, width=79, initial='', subsequent=''):
     output = textwrap.fill(line, width, initial_indent=initial, 
                           subsequent_indent=subsequent)
     print(output)
 
 def main():
-    utils = "/usr/local/etc/utils.txt"
-    with open(utils, 'rU') as infile:
-        json_data = json.load(infile)
     args = argument_parser().parse_args()
+    utils = args.database
+    with open(utils, 'rU') as in_h:
+        json_data = json.load(in_h)
     args.func(args, json_data)
 
 if __name__ == "__main__":
