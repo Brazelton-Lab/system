0af7cfba034504854fd81c6c1299a605fdbf8b4d
Christopher Thornton <christopher.thornton@utah.edu>
2015-08-06 11:49:27 -0600

    add snapshot creation

--- backup.sh
+++ backup.sh
@@ -3,9 +3,10 @@
 
 # functions
 write_log() {
-    date=$(date +"%b %-d %k:%M:%S")
-    host=$(hostname -s)
-    echo "$date $host $script: $1" >> $log_file;
+    TIMESTAMP=$(date +"%b %-d %k:%M:%S")
+    LHOST=$(hostname -s)
+    SCRIPT=$(basename $0);
+    echo "$TIMESTAMP $LHOST $SCRIPT: $1";
 }
 
 show_help() {
@@ -26,14 +27,30 @@ optional arguments:
 EOF
 }
 
+remove_snapshot() {  
+    if ! lvremove -f $1 >/dev/null 2>&1; then
+        write_log "cannot remove the lvm snapshot"
+    fi
+    exit $2
+}
+
+PATH="${PATH}:/usr/sbin:/sbin:/usr/bin:/bin";
+
+# only run as root
+if [ "$(id -u)" != '0' ]; then
+    echo "this script has to be run as root";
+    exit 1
+fi
+
 # variables
-ssh=$(which ssh);
-basename=$(which basename);
-script=$($basename $0)
-remote_host='root@baas-becking.biology.utah.edu';
-port=53213;
-log_file='/var/log/rsync';
-exclude='lost+found';
+RHOST='root@baas-becking.biology.utah.edu';
+PORT=53213;
+LOG='/var/log/rsync';
+EXCLUDE='lost+found';
+LOGVOL='';
+VOLGROUP='vg_winogradsky';
+SNAPSIZE='40GiB';
+SNAPNAME='lv_snap';
 
 # parse command line arguments
 OPTIND=1;
@@ -43,13 +60,17 @@ while getopts "h?r:p:l:e:" opt; do
             show_help;
             exit 0
             ;;
-        r)  remote_host=$OPTARG
+        r)  RHOST=$OPTARG
             ;;
-        p)  port=$OPTARG
+        p)  PORT=$OPTARG
             ;;
-        l)  log_file=$OPTARG
+        l)  LOG=$OPTARG
             ;;
-        e)  exclude=$OPTARG
+        e)  EXCLUDE=$OPTARG
+            ;;
+        v)  LOGVOL=$OPTARG
+            ;;
+        g)  VOLGROUP=$OPTARG
             ;;
         ?)
             show_help;
@@ -60,22 +81,22 @@ done
 
 if [ $(( $# - $OPTIND )) -lt 1 ]; then
     show_help;
-    exit 1;
+    exit 1
 fi
 
 source_dir="${@:$OPTIND:1}";
 dest_dir="${@:$OPTIND+1:1}";
 
-# verify that user input is valid
+# verify that source is a valid object
 if [ ! -e "$source_dir" ]; then
     write_log "source $source_dir is not a valid file or directory";
-    exit 1;
+    exit 1
 else
-    write_log "starting backup of $source_dir";
+    write_log "starting backup of $source_dir"
 fi
 
 # obtain destination link
-link=$($ssh -q -p $port $remote_host find $dest_dir -maxdepth 1 -type d | sort -n | tail -1);
+link=$(/usr/bin/ssh -q -p $PORT $RHOST find $dest_dir -maxdepth 1 -type d | sort -n | tail -1 2>/dev/null);
 if [ -z "$link" ]; then
     write_log "backup failed: unable to obtain a destination link";
     exit 1;
@@ -87,24 +108,49 @@ destination="$dest_dir/$(date -I)";
 
 # verify that destination does not already exist on the remote server
 if [ $destination == $link ]; then
-    write_log "destination directory $destination already exists on remote machine";
+    write_log "backup failed: destination directory $destination already exists on remote machine";
     exit 1;
 fi
 
+if [ ! -z "$LOGVOL"]; then
+    snapshot="/dev/${VOLGROUP}/${SNAPNAME}";
+    # check that the snapshot does not already exist
+    if [ -e "$snapshot" ]; then
+        write_log "backup failed: snapshot $SNAPNAME already exists";
+        exit 1;
+    else
+        write_log "creating snapshot $snapshot and mounting it at $source_dir";
+    fi
+    # create the snapshot
+    if ! lvcreate --size ${SNAPSIZE} --snapshot --name ${SNAPNAME} $snapshot >/dev/null 2>&1; then
+        write_log "backup failed: unable to create snapshot of /dev/${VOLGROUP}/${LOGVOL}";
+        exit 1;
+    fi
+    # mount snapshot at source
+    if ! mount -ro $snapshot $source_dir >/dev/null 2>&1; then
+        write_log "backup failed: unable to mount $snapshot at $source_dir";
+        remove_snapshot $snapshot 1;
+    fi
+fi
+
 # backup data
-write_log "using rsync to backup data";
-sync_err=$(/usr/bin/rsync -e "ssh -q -p $port" -azAO --no-o --no-g --log-file=$log_file --exclude=$exclude --delete --link-dest=$link $source_dir $remote_host:$destination 2>&1 >/dev/null);
+sync_err=$(rsync -e "ssh -q -p $PORT" -azAO --no-o --no-g --log-file=$LOG --exclude=$EXCLUDE --delete --link-dest=$link $source_dir $RHOST:$destination 2>&1 >/dev/null);
 if [ -n "$sync_err" ]; then
     write_log "backup failed: $1";
-    rmdir_err=$($ssh -q -p $port $remote_host rm -Rf $destination 2>&1 > /dev/null);
+    rmdir_err=$(ssh -q -p $PORT $RHOST rm -Rf $destination 2>&1 > /dev/null);
     if [ -n "$rmdir_err" ]; then
         write_log "unable to remove $destination: $rmdir_err";
     else
         write_log "destination directory $destination successfully removed";
     fi
-    exit 1;
+    if [ ! -z $snapshot ]; then
+        remove_snapshot $snapshot 1;
+    else
+        exit 1;
 fi
 
 write_log "backup successful";
-
-exit 0;
+if [ ! -z $snapshot ]; then
+    remove_snapshot $snapshot 0;
+else
+    exit 0;
