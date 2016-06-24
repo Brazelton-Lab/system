98efad252405d769aa112f6dd048150bf6df7994
Christopher Thornton <christopher.thornton@utah.edu>
2016-06-24 03:17:50 -0600

    Added additional error catching at most major steps

--- backup.sh
+++ backup.sh
@@ -6,54 +6,67 @@ write_log() {
     TIMESTAMP=$(date +"%b %-d %k:%M:%S");
     LHOST=$(hostname -s);
     SCRIPT=$(basename $0);
-    echo "$TIMESTAMP $LHOST $SCRIPT: $1" >> $LOG;
+    if [ ! -z $LOG ]; then
+        echo "$TIMESTAMP $LHOST $SCRIPT: $1" >> $LOG;
+    else
+        echo "$TIMESTAMP $LHOST $SCRIPT: $1"
+    fi
 }
 
 show_help() {
 cat << EOF
-Usage: ${0##*/} [-h] [-r HOST] [-l LOG] [-p PORT] [-e FILES] [-v LV] [-g VG] source destination
+Usage: ${0##*/} [-h] [-d] [-r HOST] [-l LOG] [-p PORT] [-e FILES] [-v LV] \
+    [-g VG] [-s SIZE] source destination
 Brazelton Lab backup script
 
 positional arguments:
   source          directory to be backed up
-  destination     destination on remote machine
+  destination     destination directory on remote machine
 
 optional arguments:
   -h              display this help message and exit
-  -r HOST         account on remote host
+  -d              do not require a destination link [default: link required]
+  -r HOST         account on remote host (user@remote_address)
   -l LOG          log file
-  -p PORT         ssh port
+  -p PORT         ssh port on remote machine [default: 22]
   -e FILE         text file of excludes
   -v LV           lvm logical volume
   -g VG           lvm volume group
+  -s SIZE         snapshot size [default: 200GiB]
 EOF
 }
 
 remove_snapshot() { 
+    write_log "cleaning up snapshot $1";
     if ! lvremove -f $1 >/dev/null 2>&1; then
-        write_log "cannot remove the lvm snapshot";
+        write_log "error: cannot remove the lvm snapshot";
+        exit 1
+    else
+        write_log "snapshot successfully removed";
     fi
 }
 
 PATH="${PATH}:/usr/sbin:/sbin:/usr/bin:/bin";
 
 # variables
-RHOST='root@baas-becking.biology.utah.edu';
-PORT=53213;
-LOG='/var/log/rsync';
-EXCLUDE='/usr/local/etc/excludes.txt';
+RHOST='';
+PORT=22;
+LOG='';
+EXCLUDES='';
 LOGVOL='';
-VOLGROUP='vg_winogradsky';
-SNAPSIZE='40GiB';
-SNAPNAME='lv_snap';
+VOLGROUP='';
+REQUIRE_LINK=true;
+SNAPSIZE='200GiB';
 
 # parse command line arguments
 OPTIND=1;
-while getopts "h?r:p:l:e:v:g:" opt; do
+while getopts "h?r:p:l:e:v:g:s:" opt; do
     case "$opt" in
         h)
             show_help;
             exit 0
+            ;;
+        d)  REQUIRE_LINK=false
             ;;
         r)  RHOST=$OPTARG
             ;;
@@ -61,11 +74,13 @@ while getopts "h?r:p:l:e:v:g:" opt; do
             ;;
         l)  LOG=$OPTARG
             ;;
-        e)  EXCLUDE=$OPTARG
+        e)  EXCLUDES=$OPTARG
             ;;
         v)  LOGVOL=$OPTARG
             ;;
         g)  VOLGROUP=$OPTARG
+            ;;
+        s)  SNAPSIZE=$OPTARG
             ;;
         ?)
             show_help;
@@ -80,60 +95,125 @@ fi
 source_dir="${@:$OPTIND:1}";
 dest_dir="${@:$OPTIND+1:1}";
 
-# verify that source is a valid object
-if [ ! -e "$source_dir" ]; then
-    write_log "source $source_dir is not a valid file or directory";
-    exit 1;
-else
-    write_log "starting backup of $source_dir";
+# verify that the log file exists and is writable
+if [ ! -z "$LOG" ]; then
+    if [ ! -e "$LOG" ]; then
+        if ! touch $LOG; then
+            echo "error: unable to create $LOG";
+            exit 1
+        fi
+    else
+        if [ ! -w "$LOG" ]; then
+            echo "error: unable to write to log";
+            exit 1
+        fi
+    fi
 fi
+
+write_log "starting backup of $source_dir";
+
+# verify that dependant arguments are used together
+if [[ ( -z "$VOLGROUP"  && ! -z "$LOGVOL" ) || ( -z "$LOGVOL" && ! -z "$VOLGROUP" ) ]]; then
+    write_log "error: arguments -g and -v must be used together";
+    exit 1
+fi
+
+# Set the destination
+destination="$dest_dir/$(date -I)";
 
 # obtain destination link
 link=$(/usr/bin/ssh -q -p $PORT $RHOST find $dest_dir -maxdepth 1 -type d | sort -n | tail -1 2>/dev/null);
 if [ -z "$link" ]; then
-    write_log "backup failed: unable to obtain a destination link";
-    exit 1;
+    if [ "$REQUIRE_LINK" == true ]; then
+        write_log "error: unable to obtain a destination link";
+        exit 1
+    else
+        write_log "warning: destination link not found but require destination link is set to false. Will perform a full backup";
+        link_dest='';
+    fi
 else
-    write_log "found destination link: $link";
+    write_log "using destination link ${link} for incremental backup";
+    link_dest="--link-dest=${link}";
 fi
 
-destination="$dest_dir/$(date -I)";
-
-# verify that destination does not already exist on the remote server
+# verify that destination is not also the link (in case backup occurs twice in one day)
 if [ $destination == $link ]; then
-    write_log "backup failed: destination directory $destination already exists on remote machine";
-    exit 1;
+    write_log "error: destination directory $destination already exists on remote machine. Please delete";
+    exit 1
 fi
 
+# If requested, create the snapshot and mount it at the source directory
 if [ ! -z "${LOGVOL}" ]; then
-    snapshot="/dev/${VOLGROUP}/${SNAPNAME}";
+    snapname="${LOGVOL}_snap";
+    snapshot="/dev/${VOLGROUP}/${snapname}";
     # check that the snapshot does not already exist
     if [ -e "$snapshot" ]; then
-        write_log "backup failed: snapshot $SNAPNAME already exists";
-        exit 1;
+        write_log "error: snapshot $snapname already exists";
+        exit 1
     else
-        write_log "creating snapshot $snapshot and mounting it at $source_dir";
+        write_log "creating snapshot $snapshot and mounting it at $source_dir"
     fi
     # create the snapshot
-    if ! lvcreate --size ${SNAPSIZE} --snapshot --name ${SNAPNAME} /dev/${VOLGROUP}/${LOGVOL} >/dev/null 2>&1; then
-        write_log "backup failed: unable to create snapshot of /dev/${VOLGROUP}/${LOGVOL}";
-        exit 1;
+    if ! lvcreate --size ${SNAPSIZE} --snapshot --name ${snapname} /dev/${VOLGROUP}/${LOGVOL} >/dev/null 2>&1; then
+        write_log "error: unable to create a snapshot of ${LOGVOL}";
+        exit 1
     fi
     # mount snapshot at source
-    if ! mount $snapshot $source_dir >/dev/null 2>&1; then
-        write_log "backup failed: unable to mount $snapshot at $source_dir";
-        remove_snapshot $snapshot;
-        exit 1;
+    if [ -d "$source_dir" ]; then # check existence
+         if [ "$(ls -A $source_dir)" ]; then # fail if not empty
+             write_log "error: source directory ${source_dir} already exists and is non-empty";
+             remove_snapshot ${snapshot};
+             exit 1
+         fi
+    else
+        if ! mkdir ${source_dir}; then 
+            write_log "error: unable to create the source directory $source";
+            remove_snapshot ${snapshot};
+            exit 1
+        fi
     fi
+    if ! mount ${snapshot} ${source_dir} >/dev/null 2>&1; then
+        write_log "error: unable to mount $snapshot at $source_dir";
+        remove_snapshot ${snapshot};
+        exit 1
+    fi
+else
+    # otherwise verify that source is a valid file or directory
+    if [ ! -e "$source_dir" ]; then
+        write_log "error: source $source_dir is not a valid file or directory";
+        exit 1
+    fi
+fi
+
+# Set rsync arguments
+RSYNCOPTS=(-azAX --no-o --no-g --delete ${link_dest});
+if [ ! -z $EXCLUDES ]; then
+    RSYNCOPTS+=(--exclude-from=${EXCLUDES});
+fi
+
+if [ ! -z $LOG ]; then
+    RSYNCOPTS+=(--log-file=${LOG});
+fi
+
+if [ ! -z $RHOST ]; then
+    RSYNCOPTS+=(--rsh="ssh -q -p ${PORT}");
+    backup_dest="${RHOST}:${destination}";
+else
+    backup_dest=$destination
 fi
 
 # backup data
-sync_err=$(rsync -e "ssh -q -p $PORT" -azAO --no-o --no-g --log-file=$LOG --exclude-from=$EXCLUDE --delete --link-dest=$link $source_dir $RHOST:$destination 2>&1 >/dev/null);
+sync_err=$(rsync "${RSYNCOPTS[@]}" ${source_dir} ${backup_dest} 2>&1 >/dev/null);
+#print_statement="rsync ${RSYNCOPTS[@]} ${source_dir} ${backup_dest}";
+#echo $print_statement;
+#echo
+
+# remove snapshot if it exists
+if [ ! -z $snapshot ]; then
+    umount -f ${source_dir};
+    remove_snapshot ${snapshot};
+fi
 
 write_log "backup finished";
-if [ ! -z $snapshot ]; then
-    umount -f $source_dir;
-    remove_snapshot $snapshot;
-fi
 
 exit 0
