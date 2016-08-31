29613bacd90cbd5c24ad0475c20025b2163a8498
TheOneHyer <theonehyer@gmail.com>
2016-08-31 10:32:44 -0600

    Added integrity_audit.py and ChangeLog.rst
    Added ChangeLog.rst for use by gitchangelog
    and added metadata for integrity_audit.py

---
+++ integrity_audit.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+
+from __future__ import print_function
+
+"""Verify data integrity via checksums
+
+
+"""
+
+__author__ = 'Alex Hyer'
+__credits__ = 'Christopher Thornton'
+__email__ = 'theonehyer@gmail.com'
+__license__ = 'GPLv3'
+__maintainer__ = 'Alex Hyer'
+__status__ = 'Alpha'
+__version__ = '0.0.1'
+
+import argparse
+import os
+import sys
