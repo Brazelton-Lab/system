80080488f8a802c67bda8d32c48160c74e8d818f
TheOneHyer <theonehyer@gmail.com>
2016-08-31 18:08:50 -0600

    Added Ability to get File Structure Info
    Added Classes and main function loop to obtain
    info on directory structure.

---
+++ ChangeLog.rst
@@ -0,0 +1,114 @@
+Changelog
+=========
+
+%%version%% (unreleased)
+------------------------
+
+- Added integrity_audit.py and ChangeLog.rst. [TheOneHyer]
+
+  Added ChangeLog.rst for use by gitchangelog
+  and added metadata for integrity_audit.py
+
+- Remove utils.txt. [Christopher Thornton]
+
+- Rename utils.txt to utils.json. [Christopher Thornton]
+
+- Add argument -d to list of arguments to parse. [Christopher Thornton]
+
+- Added additional error catching at most major steps. [Christopher
+  Thornton]
+
+- Change subcommand display to show. [Christopher Thornton]
+
+- Fixed utils.py append mode. [Alex Hyer]
+
+- Added autocomplete to prgram editing. [Alex Hyer]
+
+- Added ability to specify multiple categories in utils.py. [Alex Hyer]
+
+- Added autocomplete feature to utils.py. [Alex Hyer]
+
+- Added category viewing options to utils.py. [Alex Hyer]
+
+- Fixed bug with previous version in utils.py. [Alex Hyer]
+
+- Added categories to utils.py and added manually curated list for
+  editing items under relevant_values. [Alex Hyer]
+
+- Remove deletion of backup when rsync fails. [Christopher Thornton]
+
+- Add snapshot creation. [Christopher Thornton]
+
+- Cleanup output. [Christopher Thornton]
+
+- Merge branch 'master' of
+  ssh://winogradsky.biology.utah.edu:53211/srv/repos/system.
+  [Christopher Thornton]
+
+- Modify how utils accepts multiple arguments. [Christopher Thornton]
+
+- Correct location for config file. [Christopher Thornton]
+
+- Return correct thing from match_test. [Christopher Thornton]
+
+- Fix punctuation error. [Christopher Thornton]
+
+- Fix spelling error in match_test. [Christopher Thornton]
+
+- Modify how utils checks for existing programs in the database.
+  [Christopher Thornton]
+
+- Remove dependency on snapshots and add additional error checking.
+  [Christopher Thornton]
+
+- Make log file optional. [Christopher Thornton]
+
+- Merge branch 'master' of /./srv/repos/system. [Alex Hyer]
+
+- Add functions to check success/failure. [Christopher Thornton]
+
+- Update utils.txt. [Christopher Thornton]
+
+- Heavily Modified integrity_check.py. [Alex Hyer]
+
+  The core functionality of integrity_check.py remains unchanged.
+  integrity_check.py now parallelizes checking the core function
+  of computing and checking checksums. Additoinally, the program
+  now outputs to a user-defined log file instead of syslog.
+  integrity_check.py command line now requries three arguments:
+
+  integrity_check.py directory_to_analyze log_file core_number
+
+  If core_number is unspecified, it defaults to one.
+
+- Add class to default to usage message. [Christopher Thornton]
+
+- Change path to text file. [Christopher Thornton]
+
+- Finish edit subcommand. [Christopher Thornton]
+
+- Subcommands for different desired action. [Christopher Thornton]
+
+- Ignore case when searching for specific programs. [Christopher
+  Thornton]
+
+- Fixed plurality inconsistencies in usage message. [Christopher
+  Thornton]
+
+- Fix to comply with standard conventions and add support for multiple
+  program input. [Christopher Thornton]
+
+- Add script to display list of bioinformatics programs to users on the
+  server. [Christopher Thornton]
+
+- Merge branch 'master' of /srv/repos/system. [root]
+
+- Finish backup script. [Christopher Thornton]
+
+- Add additional logging. [root]
+
+- Fix obtaining files from data path. [Christopher Thornton]
+
+- Initial commit. [Christopher Thornton]
+
+
--- integrity_audit.py
+++ integrity_audit.py
@@ -7,14 +7,229 @@ from __future__ import print_function
 
 """
 
+import argparse
+import os
+import sys
+
 __author__ = 'Alex Hyer'
 __credits__ = 'Christopher Thornton'
 __email__ = 'theonehyer@gmail.com'
 __license__ = 'GPLv3'
 __maintainer__ = 'Alex Hyer'
 __status__ = 'Alpha'
-__version__ = '0.0.1'
+__version__ = '0.0.1a2'
 
-import argparse
-import os
-import sys
+
+class Directory:
+    """A simple class to wrap and perform functions on files in a directory
+
+    Attributes:
+        path (str): absolute path to directory
+
+        files (list): list of File classes for files in directory
+    """
+
+    def __init__(self, path, files):
+        """Initialize attributes to store directory data"""
+
+        self.path = path
+        self.files = files
+
+    def size(self):
+        """Calculate and return size of directory in bytes
+
+        Returns:
+            int: size of all files in directory
+        """
+
+        size = 0
+        for path in self.files:
+            size += path.size
+        return size
+
+
+class File:
+    """A simple class to store file locations, checksums, mtimes, and sizes
+
+    While the data stored in this class is accessible, and often initially
+    obtained via the python os library, storing these variables in memory
+    probably reduces access time as opposed to asking the OS every time.
+    Additionally, this class-based approach definitely simplifies the code.
+
+    Attributes:
+        path (str): absolute path to file
+
+        checksum (str): checksum of file
+
+        mtime (int): time file last modified in seconds since epoch
+
+        size (int): size of file in bytes
+    """
+
+    def __init__(self, path):
+        """Initialize attributes to store file data"""
+
+        self.path = path
+        self.checksum = None
+        self.mtime = None
+        self.size = None
+
+
+# This method is literally just the Python 3.5.1 which function from the
+# shutil library in order to permit this functionality in Python 2.
+# Minor changes to style were made to account for indentation.
+def which(cmd, mode=os.F_OK | os.X_OK, path=None):
+    """Given a command, mode, and a PATH string, return the path which
+    conforms to the given mode on the PATH, or None if there is no such
+    file.
+    `mode` defaults to os.F_OK | os.X_OK. `path` defaults to the result
+    of os.environ.get("PATH"), or can be overridden with a custom search
+    path.
+    """
+
+    # Check that a given file can be accessed with the correct mode.
+    # Additionally check that `file` is not a directory, as on Windows
+    # directories pass the os.access check.
+    def _access_check(fn, mode):
+        return (os.path.exists(fn) and os.access(fn, mode)
+                and not os.path.isdir(fn))
+
+    # If we're given a path with a directory part, look it up directly
+    # rather than referring to PATH directories. This includes checking
+    # relative to the current directory, e.g. ./script
+    if os.path.dirname(cmd):
+        if _access_check(cmd, mode):
+            return cmd
+        return None
+
+    if path is None:
+        path = os.environ.get("PATH", os.defpath)
+    if not path:
+        return None
+    path = path.split(os.pathsep)
+
+    if sys.platform == "win32":
+        # The current directory takes precedence on Windows.
+        if not os.curdir in path:
+            path.insert(0, os.curdir)
+        # PATHEXT is necessary to check on Windows.
+        pathext = os.environ.get("PATHEXT", "").split(os.pathsep)
+        # See if the given file matches any of the expected path
+        # extensions. This will allow us to short circuit when given
+        # "python.exe". If it does match, only test that one, otherwise
+        # we have to try others.
+        if any(cmd.lower().endswith(ext.lower()) for ext in
+               pathext):
+            files = [cmd]
+        else:
+            files = [cmd + ext for ext in pathext]
+    else:
+        # On other platforms you don't have things like PATHEXT to tell you
+        # what file suffixes are executable, so just pass on cmd as-is.
+        files = [cmd]
+
+    seen = set()
+    for dir in path:
+        normdir = os.path.normcase(dir)
+        if not normdir in seen:
+            seen.add(normdir)
+            for thefile in files:
+                name = os.path.join(dir, thefile)
+                if _access_check(name, mode):
+                    return name
+    return None
+
+
+def main(args):
+    """Control program flow
+
+    Arguments:
+        args (ArgumentParser): args to control program options
+    """
+
+    # Obtain directory structure and data
+    dirs = []
+    args.recursive = True if args.max_depth > 0 else False  # -m implies -r
+    for root, dir_names, file_names in os.walk(args.directory):
+
+        # If directory beyond max depth, skip rest of loop
+        if root.count(os.path.sep) > args.max_depth > -1:
+            continue
+
+        norm_root = os.path.normpath(root)
+
+        # Skip hidden directories unless specified
+        if args.hidden is False:
+            parts = root.split(os.path.sep)
+            for part in parts:
+                if part[0] == '.':
+                    continue
+
+        file_classes = []
+        for file_name in file_names:
+
+            # Skip hidden files unless specified
+            if args.hidden is False and file_name[0] == '.':
+                continue
+
+            # Initiate File class and store attributes
+            file_path = os.path.join(norm_root, file_name)
+            file_class = File(file_path)
+            file_class.mtime = os.path.getmtime(file_path)
+            file_class.size = os.path.getsize(file_path)
+            file_classes.append(File(file_path))
+
+        # Initialize directory and pass File handles
+        directory = Directory(norm_root, file_classes)
+        dirs.append(directory)
+
+        # Break loop on first iteration if not recursive
+        if args.recursive is False:
+            break
+
+    # In-house tests show that, predictably, Linux *sum commands are much
+    # faster than Python's built-in hashlib. Use *sum commands when available.
+    sum_cmd = which(args.algo + 'sum')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description=__doc__,
+                                     formatter_class=argparse.
+                                     RawDescriptionHelpFormatter)
+    parser.add_argument('directory', metavar='dir',
+                        type=str,
+                        help='directory containing files to check')
+    parser.add_argument('-l', '--log',
+                        type=argparse.FileType('a'),
+                        help='log file to write output [Default: syslog]')
+    parser.add_argument('-a', '--algorithm',
+                        type=str,
+                        default='sha512',
+                        choices=['md5',
+                                 'sha1',
+                                 'sha224',
+                                 'sha256',
+                                 'sha384',
+                                 'sha512'],
+                        help='algorithm used to perform checksums')
+    parser.add_argument('-d', '--hidden',
+                        action='store_true',
+                        help='check files in hidden directories and hidden '
+                             'files')
+    parser.add_argument('-r', '--recursive',
+                        action='store_true',
+                        help='check files in all subdirectories')
+    parser.add_argument('-m', '--max_depth',
+                        type=int,
+                        default=-1,
+                        help='max number of subdirectory levels to check, '
+                             'implies "-r"')
+    parser.add_argument('-t', '--threads',
+                        type=int,
+                        default=1,
+                        help='number of threads to run check with')
+    args = parser.parse_args()
+
+    main(args)
+
+sys.exit(0)
